854a32e32c6b6624b8f7bba68cdbefea2f1d92f3
Gabriel Manika <gmkoeb@gmail.com>
2025-02-19 11:05:44 -0300

    feat: create route to get subscriber successful invites count

---
+++ src/functions/get-subscriber-invites-count.ts
@@ -0,0 +1,13 @@
+import { redis } from '../redis/client'
+
+interface getSubscriberInviteCountParams {
+  subscriberId: string
+}
+
+export async function getSubscriberInvitesCount({
+  subscriberId,
+}: getSubscriberInviteCountParams) {
+  const count = await redis.zscore('referral:ranking', subscriberId)
+
+  return { count: count ? Number.parseInt(count) : 0 }
+}
