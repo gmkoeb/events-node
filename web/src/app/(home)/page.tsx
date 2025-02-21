23cc1ae2250623f9a9a246ce528a97568cf01836
Gabriel Manika <gmkoeb@gmail.com>
2025-02-21 15:09:08 -0300

    feat: handle form data submission
    - Installed the react-hook-form package to handle form submission
    
    - Integrated zod with react hookform to handle form validation

--- web/src/app/(home)/page.tsx
+++ web/src/app/(home)/page.tsx
@@ -1,8 +1,7 @@
-import { Button } from '@/components/button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { ArrowRight, Radio, User } from 'lucide-react'
+import { Radio } from 'lucide-react'
 import Image from 'next/image'
-import logo from '../assets/logo.svg'
+import logo from '../..//assets/logo.svg'
+import { SubscriptionForm } from './subscription-form'
 
 export default function Home() {
   return (
@@ -36,33 +35,7 @@ export default function Home() {
             Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
           </p>
         </div>
-        <form
-          className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
-          action=""
-        >
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome Completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="email" placeholder="E-mail" />
-            </InputRoot>
-            <Button type="submit">
-              Confirmar
-              <ArrowRight />
-            </Button>
-          </div>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   )
--- web/src/app/invite/page.tsx
+++ web/src/app/invite/page.tsx
@@ -1,13 +1,12 @@
-import { IconButton } from '@/components/icon-button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
 import Image from 'next/image'
 import logo from '../../assets/logo.svg'
-import medal_bronze from '../../assets/medal_bronze.svg'
-import medal_gold from '../../assets/medal_gold.svg'
-import medal_silver from '../../assets/medal_silver.svg'
+import { InviteLinkInput } from './invite-link-input'
+import { Ranking } from './ranking'
+import { Stats } from './stats'
 
 export default function InvitePage() {
+  const inviteLink = 'http://localhost:3000/invite/1234103'
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -31,97 +30,11 @@ export default function InvitePage() {
               inscrições:
             </p>
           </div>
-          <InputRoot>
-            <InputIcon>
-              <Link className="size-5" />
-            </InputIcon>
-            <InputField
-              defaultValue={'http://localhost:3000/invite/123912939129'}
-              readOnly
-            />
-            <IconButton className="-mr-2">
-              <Copy className="size-5" />
-            </IconButton>
-          </InputRoot>
-
-          <div className="grid gap-3 md:grid-cols-3">
-            <div className="relative bg-gray-700 boder border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Acessos ao link
-              </span>
-              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-            <div className="relative bg-gray-700 boder border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Inscrições feitas
-              </span>
-              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-            <div className="relative bg-gray-700 boder border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                3°
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Posição no ranking
-              </span>
-              <Medal className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-          </div>
+          <InviteLinkInput inviteLink={inviteLink} />
+          <Stats />
         </div>
       </div>
-
-      <div className="w-full max-w-[440px] space-y-5">
-        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-          Ranking de indicações
-        </h2>
-
-        <div className="space-y-4">
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1°</span> | Gabriel Manika
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-
-            <Image alt="" src={medal_gold} className="absolute top-0 right-8" />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">2°</span> | Gabriel Manika
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-
-            <Image
-              alt=""
-              src={medal_silver}
-              className="absolute top-0 right-8"
-            />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">3°</span> | Gabriel Manika
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-
-            <Image
-              alt=""
-              src={medal_bronze}
-              className="absolute top-0 right-8"
-            />
-          </div>
-        </div>
-      </div>
+      <Ranking />
     </div>
   )
 }
